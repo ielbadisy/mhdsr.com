0942714ad59e89338d699ea9bdd292dd27ae88ce
ielbadisy <elbadisyimad@gmail.com>
2026-08-24 14:49:32 +0100

    Align prompt mark geometry

--- logo-mhdsr.R
+++ logo-mhdsr.R
@@ -43,9 +43,29 @@ logo <- ggplot(hex, aes(x, y)) +
   ) +
   annotate(
     "text",
-    x = 0,
+    x = -0.14,
     y = 0.06,
-    label = "MHDSR |>",
+    label = "MHDSR",
+    color = text,
+    size = 12,
+    family = "red_hat_text",
+    fontface = "plain"
+  ) +
+  annotate(
+    "segment",
+    x = 0.41,
+    xend = 0.41,
+    y = -0.01,
+    yend = 0.13,
+    color = text,
+    linewidth = 2,
+    lineend = "butt"
+  ) +
+  annotate(
+    "text",
+    x = 0.54,
+    y = 0.06,
+    label = ">",
     color = text,
     size = 12,
     family = "red_hat_text",
